ae3ed74508e5e075d854748ffe6b6b91833bd234
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-27 19:38:56 +0700

    Konversi Halaman : Sign Up

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,11 +2,12 @@
   <div class="container mx-auto h-screen flex justify-center items-center">
     <div class="w-full lg:w-1/3 px-10 lg:px-0">
       <div class="flex justify-center items-center mx-auto mt-6 mb-8">
-        <img src="/success-illustration.svg" alt="" class="w-full" />
+        <img src="/404-illustration.svg" alt="" class="w-full" />
       </div>
       <h2 class="font-medium mb-3 text-3xl text-center">
         Oops! Something went wrong.
       </h2>
+
       <p class="text-center font-light" v-if="error.statusCode === 404">
         The page that you requested doesn't
         <br />
