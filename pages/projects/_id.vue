8faed16cc84172fdcde6e0f11bf748e648e09ed1
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-29 22:05:46 +0700

    Integrasi API : Integrasi Detail Funding

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -109,19 +109,19 @@
               </div>
               <div class="w-3/4 ml-5 mt-1">
                 <div class="font-semibold text-xl text-gray-800">
-                  Julia Keeva
+                  {{ campaign.data.user.name }}
                 </div>
                 <div class="font-light text-md text-gray-400">
-                  28.093 backer
+                  {{ campaign.data.backer_count }} backer
                 </div>
               </div>
             </div>
 
             <h4 class="mt-5 font-semibold">What will you get:</h4>
             <ul class="list-check mt-3">
-              <li>Equity of the startup directly from the founder</li>
-              <li>Special service or product that startup has</li>
-              <li>You can also sell your equity once the startup going IPO</li>
+              <li v-for="perk in campaign.data.perks" :key="perk">
+                {{ perk }}
+              </li>
             </ul>
             <input
               type="number"
@@ -170,10 +170,10 @@
       <div class="flex justify-between items-center">
         <div class="w-full md:w-3/4 mr-6">
           <h2 class="text-4xl text-gray-900 mb-2 font-medium">
-            Wireboard Fortune
+            {{ campaign.data.name }}
           </h2>
           <p class="font-light text-xl mb-5">
-            The new era of mechanical keyboard for everyone
+            {{ campaign.data.short_description }}
           </p>
 
           <div class="relative progress-bar">
@@ -189,7 +189,12 @@
               "
             >
               <div
-                style="width: 80%"
+                :style="
+                  'width:' +
+                  (campaign.data.current_amount / campaign.data.goal_amount) *
+                    100 +
+                  '%'
+                "
                 class="
                   shadow-none
                   flex flex-col
@@ -204,25 +209,19 @@
             </div>
           </div>
           <div class="flex progress-info mb-6">
-            <div class="text-2xl">80%</div>
-            <div class="ml-auto font-semibold text-2xl">Rp 40.000.000</div>
+            <div class="text-2xl">
+              {{
+                (campaign.data.current_amount / campaign.data.goal_amount) *
+                100
+              }}%
+            </div>
+            <div class="ml-auto font-semibold text-2xl">
+              Rp {{ new Intl.NumberFormat().format(campaign.data.goal_amount) }}
+            </div>
           </div>
 
           <p class="font-light text-xl mb-5">
-            Designed to fit your dedicated typing experience. No matter what you
-            like, linear, clicky or a little in between, we’ve got you covered
-            with three Gateron switches options (Blue, Brown, Red). With a
-            lifespan of 50 million keystroke lifespan we want to make sure that
-            you experience same feedback for every keystroke.
-          </p>
-          <p class="font-light text-xl mb-5">
-            With N-key rollover (NKRO on wired mode only) you can register as
-            many keys as you can press at once without missing out characters.
-            It allows to use all the same media keys as conventional macOS.
-          </p>
-          <p class="font-light text-xl mb-5">
-            This keyboard can last up to 72 hours typing, or up to 9 days of
-            normal use (8 hrs/day) with a 4000 mAh big battery.
+            {{ campaign.data.description }}
           </p>
         </div>
         <div class="w-1/4 hidden md:block"></div>
@@ -234,3 +233,16 @@
     <Footer />
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    try {
+      let response = await $axios.get('/api/v1/campaigns/' + params.id)
+      return { campaign: response.data }
+    } catch (error) {
+      console.log(error)
+    }
+  },
+}
+</script>
