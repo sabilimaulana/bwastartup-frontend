7ec2a2083aa92f8c5d21e0cf5c98b3ef48c61ec6
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-30 19:40:25 +0700

    Integrasi API : Integrasi Dashboard Backer

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -44,9 +44,21 @@
       </div>
       <hr />
       <div class="block mb-2">
-        <div class="w-full lg:max-w-full lg:flex mb-4" v-for="i in 5" :key="i">
+        <div
+          class="w-full lg:max-w-full lg:flex mb-4"
+          v-for="campaign in campaigns.data"
+          :key="campaign.id"
+        >
           <div
+            :style="
+              'background-color: #bbb;background-position:center;background-image:url(\'' +
+              $axios.defaults.baseURL +
+              '/' +
+              campaign.image_url +
+              '\')'
+            "
             class="
+              border
               h-48
               lg:h-auto lg:w-48
               flex-none
@@ -60,8 +72,10 @@
               background-image: url('https://tailwindcss.com/img/card-left.jpg');
             "
           ></div>
-          <div
+          <nuxt-link
+            :to="'/dashboard/projects/' + campaign.id"
             class="
+              w-full
               border-r border-b border-l border-gray-400
               lg:border-l-0 lg:border-t lg:border-gray-400
               bg-white
@@ -75,27 +89,33 @@
           >
             <div class="mb-8">
               <div class="text-gray-900 font-bold text-xl mb-1">
-                Cari Uang Buat Gunpla
+                {{ campaign.name }}
               </div>
               <p class="text-sm text-gray-600 flex items-center mb-2">
-                Rp. 200.000.000 &middot; 80%
+                Rp.
+                {{ new Intl.NumberFormat().format(campaign.goal_amount) }}
+                &middot;
+                {{ (campaign.current_amount / campaign.goal_amount) * 100 }}%
               </p>
               <p class="text-gray-700 text-base">
-                With N-key rollover (NKRO on wired mode only) you can register
-                as many keys as you can press at once without missing out
-                characters. It allows to use all the same media keys as
-                conventional macOS.
+                {{ campaign.short_description }}
               </p>
             </div>
             <div class="flex items-center">
-              <nuxt-link
-                :to="`/dashboard/projects/${i}`"
-                class="bg-green-button text-white py-2 px-4 rounded"
+              <button
+                class="
+                  bg-green-button
+                  text-white
+                  py-2
+                  px-4
+                  rounded
+                  hover:opacity-80
+                "
               >
                 Detail
-              </nuxt-link>
+              </button>
             </div>
-          </div>
+          </nuxt-link>
         </div>
       </div>
     </section>
@@ -104,3 +124,15 @@
     <Footer />
   </div>
 </template>
+
+<script>
+export default {
+  middleware: 'auth',
+  async asyncData({ $axios, app }) {
+    const campaigns = await $axios.$get(
+      '/api/v1/campaigns?user_id=' + app.$auth.user.id
+    )
+    return { campaigns }
+  },
+}
+</script>
